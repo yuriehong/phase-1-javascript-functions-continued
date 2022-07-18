f0ffcb724607b66f45e170df16274407a4fbaba8
+ <+>
2022-07-18 15:33:06 -0400

    commit tree 91a691a0dcfeef5587bd8498347c3eb364310106

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(event="roller-skate"){
+    return `This Saturday, I want to ${event}!`;
+}
+function mondayWork(work = "go to the office"){
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(symbol = "*"){
+    return function inner(adj ="special"){
+        return `You are ${symbol}${adj}${symbol}!`;
+
+    }
+}
